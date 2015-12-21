51a3845257a3aeb1a578153d7d981a0d5206fb2a
Fred Flinstone <fflinstone@gmail.com>
2015-12-20 16:49:19 -0800

    First Commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,70 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ 
+var bio = {
+	"name": "Joe",
+	"role": "Test Lead",
+	"age": 30,
+	"url": "http://www.hartnell.edu",
+	"skills": ["awesome", "diligent", "persistent"],
+	"welcomeMessage": "Welcome to JS!",
+	"location": "Central Coast",
+	"contacts":{
+		"mobile": "831-755-5521",
+		"email" : "joe@udacity.com",
+		"twitter": "@johndoe"
+	}
+ };
+
+var work = {
+	"title": "Web Dog",
+	"employer": "Deep Compute",
+	"dates": "2006-2009",
+	"location": "Raleigh",
+	"description": "Design and Dev with JS"
+};
+
+var education = {
+	"schools" : [
+	{ 	"name": "USNA",
+		"city": "Annapolis",
+		"degree": "BS",
+		"major": "Gen Eng",
+		"year" : 1979
+	},
+	{
+		"name": "NPS",
+		"city": "Monterey",
+		"degree": "MS",
+		"major": ["Space Operations"],
+		"year": 1987
+	}
+	],
+
+	"onlineCourse" : {
+		"title": "Intro to JS",
+		"school": "Udacity",
+		"dates": 2015,
+		"url": "http://Udacity.com"
+	}
+}//end var education
+
+var projects = {
+	"title" : "Geolocation of Inexpensive ISP's",
+	"dates" : "2011-2015",
+	"description": "Cross referenced geoIp with ISP cost < 80% of mean",
+	"images": "images/map.png"
+}
+
+
+// Test Code - Commented Out
+/*
+	 $("#main").append(bio.name);
+	 $("#main").append(bio.twitter);
+	 $("#main").append(bio.location);
+
+	 $("#main").append(work["position"]);
+	 $("#main").append(education.school);
+ */
+
